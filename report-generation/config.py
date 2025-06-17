1a2ee2a7c62d4eda42c672e774f2c66956048a77
Aryan Neb <aryanneb007@gmail.com>
2025-06-17 16:25:36 -0400

    updated to remove whitespace, ported tina's work to checker

---
+++ report-generation/config.py
@@ -0,0 +1,4 @@
+# Update these absolute paths for your environment
+JDK_REPO_PATH = "/Users/aryan/Downloads/eisop/jdk"
+CHECKER_FRAMEWORK_REPO_PATH = "/Users/aryan/Downloads/eisop/checker-framework"
+RESULTS_BASE_DIR = "/Users/aryan/Downloads/eisop/jdk/report-generation/results"
